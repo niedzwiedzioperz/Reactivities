aa4ceae806f7db4f94ac5a254af716d32614a195
Mateusz Matuszewski <niedzwiedzioperz@dobrytata.pl>
2019-11-11 10:53:51 +0100

    Initial Commit

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> _logger;
+        private readonly DataContext _dataContext;
+
+        public ValuesController(
+            ILogger<ValuesController> logger,
+            DataContext dataContext)
+        {
+            _logger = logger;
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public ActionResult<IAsyncEnumerable<Value>> Get()
+            => Ok(_dataContext.Values);
+
+        [HttpGet("{id}")]
+        public ActionResult<Value> Get(int id)
+            => Ok(_dataContext.Values.FirstOrDefaultAsync(v => v.Id == id));
+    }
+}
